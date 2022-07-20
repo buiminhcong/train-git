b2cc78eb63b4707f0e82a86c9fb20136c49cfb54
cong9c6s <93995859+buiminhcong@users.noreply.github.com>
2022-07-20 13:41:32 +0700

    cong edti 2

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,7 @@ package main;
 public class Main {
     public static void main(String[] args) {
         System.out.println("DUc");
-        System.out.println("Duc");
+        System.out.println("Duc 2");
         System.out.println("Duc");
     }
     public static int tinhTong(int a,int b) {
